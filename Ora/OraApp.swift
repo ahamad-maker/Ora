3d7b638daeea808a42a65cf7d13f0da20adccc58
ahamad-maker <Aws.hamad@outlook.com>
2025-08-16 00:57:46 +0100

    Initial Commit

---
+++ Ora/OraApp.swift
@@ -0,0 +1,17 @@
+//
+//  OraApp.swift
+//  Ora
+//
+//  Created by Aws Hamad on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct OraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OraTests/OraTests.swift
@@ -0,0 +1,17 @@
+//
+//  OraTests.swift
+//  OraTests
+//
+//  Created by Aws Hamad on 16/08/2025.
+//
+
+import Testing
+@testable import Ora
+
+struct OraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
